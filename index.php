be0be9989bac8d7bc77a5cedd3246f2f7f6a6807
csdwebpro <csdwebpro@gmail.com>
2025-12-23 15:18:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
+
+
+
+<?php
+require_once(__DIR__."mojoAuthAPI.php");
+$mojoAuth = new mojoAuthAPI("c46c747d-7aa4-44e3-8bed-923b760b96bd");
+//Step 1 Get Public Key / Certificate from MojoAuth Server
+$result = $mojoAuth->getPublicKey();
+$publicKey = json_decode($result);
+//Step 2 Pass JWT token and publickey to verify user
+$userProfileData =$mojoAuth->getUserProfileData($access_token,$publicKey->data)
+?>
 // notion_notes_complete_fixed.php - Complete Fixed Notes App
 session_start();
 
